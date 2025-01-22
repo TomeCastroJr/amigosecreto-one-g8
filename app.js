2d857ca61ae7c92f8484ced8f157724fc92a64c1
Jorge Tomé <jorge.tomejr@outlook.com>
2025-01-22 14:44:06 -0400

    criação da função adicionar amigo

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ const listaDeAmigoshtml = document.getElementById("listaAmigos");
 const resultado = document.getElementById("resultado");
 
 let listaDeAmigos = [];
+
+function adicionarAmigo(){
+    const nome = nomeAmigo.value.trim();
+    if(nome === ""){
+        alert("Adicione Amigos a sua lista");
+        return;
+    }
+    listaDeAmigos.push(nome);
+    nomeAmigo.value = "";
+    atualizarListaDeAmigosHtml();
+    nomeAmigo.focus();
+}
+
